2748856a1a0bf9d6ca94bc9c00bd1a4e83a84415
Adrixn23 <Adrixndeveloper29@gmail.com>
2026-09-10 17:15:29 -0400

    CreacionEntidad

---
+++ src/Infrastructure/EnterpriseRag.Infrastructure.Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseRag.Infrastructure.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid> 
+    {
+        public string UserName;
+        public Guid TenantId;
+
+        public DateTime FechaCreacion; 
+
+    }
+}
