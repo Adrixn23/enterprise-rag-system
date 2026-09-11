10a7db8e38f126af78ad131eb16922c813c2502d
Adrixn23 <Adrixndeveloper29@gmail.com>
2026-09-11 01:12:02 -0400

    "feat(identity): implementa entidades, seeds, dtos, jwt settings y contrato IAccountService"

---
+++ src/Infrastructure/EnterpriseRag.Infrastructure.Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+ using EnterpriseRag.Core.Domain.Enums;
+
+namespace EnterpriseRag.Infrastructure.Identity.Seeds
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(RoleManager<IdentityRole<Guid>> roleManager) {
+            if (!await roleManager.RoleExistsAsync(Roles.SuperAdmin.ToString()))
+            {
+                await roleManager.CreateAsync(new IdentityRole<Guid>(Roles.SuperAdmin.ToString()));
+            }
+            if (!await roleManager.RoleExistsAsync(Roles.Admin.ToString()))
+            {
+                await roleManager.CreateAsync(new IdentityRole<Guid>(Roles.Admin.ToString()));
+
+            }
+            if (!await roleManager.RoleExistsAsync(Roles.Operador.ToString()))
+            {
+                await roleManager.CreateAsync(new IdentityRole<Guid>(Roles.Operador.ToString()));
+
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ src/Infrastructure/EnterpriseRag.Infrastructure.Identity/Seeds/DefaultSuperAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using EnterpriseRag.Core.Domain.Enums;
+using EnterpriseRag.Infrastructure.Identity.Entities;
+namespace EnterpriseRag.Infrastructure.Identity.Seeds
+{
+    public static class DefaultSuperAdmin
+    {
+        public static  async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin",
+                Email = "superadmin@rag.com",
+                FullName = "Super Administrator",
+                TenantId = Guid.NewGuid(),
+                EmailConfirmed = true
+            };
+            var user = await userManager.FindByEmailAsync(defaultUser.Email);
+
+            if (user == null)
+            {
+                await userManager.CreateAsync(defaultUser, "!Admin123");
+
+                await userManager.AddToRoleAsync(defaultUser, Roles.SuperAdmin.ToString());
+                await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                await userManager.AddToRoleAsync(defaultUser, Roles.Operador.ToString());
+            }
+        }
+
+    }
+}
