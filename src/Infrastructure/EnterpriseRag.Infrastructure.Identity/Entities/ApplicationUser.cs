ad8403a3ba546c70d031549af8f536557d26a68a
Adrixn23 <Adrixndeveloper29@gmail.com>
2026-09-10 22:47:20 -0400

    Refactor ApplicationUser: properties, naming, cleanup
    Refactored ApplicationUser to use properties with getters/setters instead of public fields. Added nullable FullName property. Renamed FechaCreacion to CreatedAt and initialized with current UTC time. Removed unused usings and added namespace.

--- src/Infrastructure/EnterpriseRag.Infrastructure.Identity/Entities/ApplicationUser.cs
+++ src/Infrastructure/EnterpriseRag.Infrastructure.Identity/Entities/ApplicationUser.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EnterpriseRag.Infrastructure.Identity.Entities
 {
     public class ApplicationUser : IdentityUser<Guid> 
     {
-        public string UserName;
-        public Guid TenantId;
+        public string? FullName { get; set; }
+        public Guid TenantId { get; set; }
 
-        public DateTime FechaCreacion; 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
 
     }
 }
